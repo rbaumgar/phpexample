9a70568f3a4a94afdd98173904d4ad5ef5d43ed0
Robert Baumgartner <robert.baumgartner@gmx.at>
2017-01-27 13:06:58 +0100

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -1,30 +1,31 @@
 <?php
-$servername = "10.1.4.11";
+$servername = getenv('MYSQL_SERVICE_HOST');
 $username = "myuser";
 $password = "mypass";
 $dbname = "mydb";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// sql to create table
-$sql = "CREATE TABLE MyGuests (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-reg_date TIMESTAMP
-)";
+    // sql to create table
+    $sql = "CREATE TABLE MyGuests (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    email VARCHAR(50),
+    reg_date TIMESTAMP
+    )";
 
-if ($conn->query($sql) === TRUE) {
+    // use exec() because no results are returned
+    $conn->exec($sql);
     echo "Table MyGuests created successfully";
-} else {
-    echo "Error creating table: " . $conn->error;
-}
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
 
-$conn->close();
+$conn = null;
 ?> 
