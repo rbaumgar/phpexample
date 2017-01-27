ed324ba77e0a94635008ff4c5a4ae9938ce636d7
Robert Baumgartner <robert.baumgartner@gmx.at>
2017-01-27 13:08:50 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,24 +1,25 @@
  <?php
-$servername = "10.1.4.11";
+$servername = getenv('MYSQL_SERVICE_HOST');
 $username = "myuser";
 $password = "mypass";
 $dbname = "mydb";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
-VALUES ('John', 'Doe', 'john@example.com')";
-
-if ($conn->query($sql) === TRUE) {
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+    VALUES ('John', 'Doe', 'john@example.com')";
+    // use exec() because no results are returned
+    $conn->exec($sql);
     echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
 
-$conn->close();
+$conn = null;
+
+<br><a href="/">Index</a>
 ?> 
